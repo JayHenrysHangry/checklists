2b610bf8ec2d88112785e55841a91bc0f1792c59
JayHenrysHangry <jay.henry@buzzfeed.com>
2017-10-19 14:33:17 -0400

    Refresher on source tree/command line

--- ChecklistsUpdated/ChecklistViewController.swift
+++ ChecklistsUpdated/ChecklistViewController.swift
@@ -21,7 +21,7 @@ class ChecklistViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView,
                                numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 100
     }
     
     override func tableView(_ tableView: UITableView,
@@ -31,6 +31,33 @@ class ChecklistViewController: UITableViewController {
             let cell = tableView.dequeueReusableCell(
                 withIdentifier: "ChecklistItem",
                 for: indexPath)
+            
+            let label = cell.viewWithTag(1000) as! UILabel
+            
+            /*if indexPath.row == 0 {
+                label.text = "Walk the dog"
+            } else if indexPath.row == 1 {
+                label.text = "Brush my teeth"
+            } else if indexPath.row == 2 {
+                label.text = "Lean iOS development"
+            } else if indexPath.row == 3 {
+                label.text = "Soccer practice"
+            } else if indexPath.row == 4 {
+                label.text = "Eat ice cream"
+            }*/
+            
+            if indexPath.row % 5 == 0 {
+                label.text = "Walk the dog"
+            } else if indexPath.row % 5 == 1 {
+                label.text = "Brush my teeth"
+            } else if indexPath.row % 5 == 2 {
+                label.text = "Learn iOS development"
+            } else if indexPath.row % 5 == 3 {
+                label.text = "Soccer practice"
+            } else if indexPath.row % 5 == 4 {
+                label.text = "Eat ice cream"
+            }
+            
             return cell
     }
 
