2d3d4c01796976860121ddf9a8d7c240d927f862
JayHenrysHangry <jay.henry@buzzfeed.com>
2017-10-26 14:28:03 -0400

    PR changes

--- ChecklistsUpdated/ChecklistViewController.swift
+++ ChecklistsUpdated/ChecklistViewController.swift
@@ -13,11 +13,6 @@ class ChecklistViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     override func tableView(_ tableView: UITableView,
                                numberOfRowsInSection section: Int) -> Int {
