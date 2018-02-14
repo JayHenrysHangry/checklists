cce2abc1d93a18a512e184b4a93381543a5c42e3
JayHenrysHangry <jay.henry@buzzfeed.com>
2018-02-14 11:14:16 -0500

    Added code cleanup for checklist toggling

---
+++ ChecklistsUpdated/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  ChecklistsUpdated
+//
+//  Created by Jay Henry on 2/14/18.
+//  Copyright © 2018 Jay Henry. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
