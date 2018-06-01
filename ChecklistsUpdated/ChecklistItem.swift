12893cda3bf089860b48f2e1431f40120446cc40
JayHenrysHangry <jay.henry@buzzfeed.com>
2018-06-01 15:26:08 -0400

    change to programmatic

--- ChecklistsUpdated/ChecklistItem.swift
+++ ChecklistsUpdated/ChecklistItem.swift
@@ -12,3 +12,20 @@ class ChecklistItem {
     var text = ""
     var checked = false
 }
+
+class MockData {
+    var checklistItems = [ChecklistItem]()
+    
+
+    init() {        
+        let row0item = ChecklistItem()
+        row0item.text = "Walk the dog"
+        row0item.checked = true
+        checklistItems.append(row0item)
+        
+        let row1item = ChecklistItem()
+        row1item.text = "Walk the cat"
+        row1item.checked = false
+        checklistItems.append(row1item)
+    }
+}
--- ChecklistsUpdated/ChecklistViewController.swift
+++ ChecklistsUpdated/ChecklistViewController.swift
@@ -9,98 +9,57 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    let mockData = MockData()
     
-    let row0text = "Walk the dog"
-    let row1text = "Brush teeth"
-    let row2text = "Learn iOS development"
-    let row3text = "Soccer practice"
-    let row4text = "Eat ice cream"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Constant.CellReuseID)
+    }
     
-    var row0checked = false
-    var row1checked = false
-    var row2checked = false
-    var row3checked = false
-    var row4checked = false
-
     private struct Constant {
         static let NumberOfRows = 5
         static let CellReuseID = "ChecklistItem"
         static let CellLabelTag = 1000
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, 
+                            numberOfRowsInSection section: Int) -> Int {
 
-        return Constant.NumberOfRows
+        return mockData.checklistItems.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             
-            let cell = tableView.dequeueReusableCell(
+            let cell = tableView.dequeueReusableCell(withIdentifier: Constant.CellReuseID,
+                                                     for: indexPath)
 
-                withIdentifier: Constant.CellReuseID, for: indexPath) //is this strange formatting?
-
-            if let label = cell.viewWithTag(Constant.CellLabelTag) as? UILabel {
-
-                if indexPath.row == 0 {
-                    label.text = row0text
-                } else if indexPath.row == 1 {
-                    label.text = row1text
-                } else if indexPath.row == 2 {
-                    label.text = row2text
-                } else if indexPath.row == 3 {
-                    label.text = row3text
-                } else if indexPath.row == 4 {
-                    label.text = row4text
-                }
-            
+            if let label = cell.textLabel {
+                let checklistItem = mockData.checklistItems[indexPath.row]
+                label.text = checklistItem.text
             }
-        configureCheckmark(for: cell, at: indexPath)
+        configureCheckmark(for: cell, at: indexPath, didTap: false)
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
         if let cell = tableView.cellForRow(at: indexPath) {
-            if indexPath.row == 0 {
-                row0checked = !row0checked
-            } else if indexPath.row == 1 {
-                row1checked = !row1checked
-            } else if indexPath.row == 2 {
-                row2checked = !row2checked
-            } else if indexPath.row == 3 {
-                row3checked = !row3checked
-            } else if indexPath.row == 4 {
-                row4checked = !row4checked
-            }
-            
-            configureCheckmark(for: cell, at: indexPath)
-            
+
+            configureCheckmark(for: cell, at: indexPath, didTap: true)
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-    func configureCheckmark(for cell: UITableViewCell, at indexPath: IndexPath) {
-        var isChecked = false
-        
-        if indexPath.row == 0 {
-            isChecked = row0checked
-        } else if indexPath.row == 1 {
-            isChecked = row1checked
-        } else if indexPath.row == 2 {
-            isChecked = row2checked
-        } else if indexPath.row == 3 {
-            isChecked = row3checked
-        } else if indexPath.row == 4 {
-            isChecked = row4checked
+    func configureCheckmark(for cell: UITableViewCell, at indexPath: IndexPath, didTap: Bool) {
+        let checklistItem = mockData.checklistItems[indexPath.row]
+        if didTap {
+            checklistItem.checked = !checklistItem.checked
         }
         
-        if isChecked {
+        if checklistItem.checked {
             cell.accessoryType = .checkmark
         } else {
             cell.accessoryType = .none
         }
     }
-
 }
-
-
